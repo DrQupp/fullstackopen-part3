962ab920c4e94cd514275d45a71925a4faf7e47e
Qupp <qupp.sub@gmail.com>
2024-08-22 22:55:24 +0900

    Add dist, use dist as root page, add vscode requests for online app

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const cors = require('cors')
 app.use(cors())
 
 
+app.use(express.static('dist'))
+
 let persons = [
   { 
     "id": "1",
@@ -117,7 +119,7 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
